053caaf07ab77cb6f97f24d63127077912a6b60d
dcZhangTW <dczhang@thoughtworks.com>
2020-01-13 16:23:56 +0800

    [Test-1] single move instruction

--- src/main/java/com/thoughtworks/marsrover/MarsRover.java
+++ src/main/java/com/thoughtworks/marsrover/MarsRover.java
@@ -5,14 +5,15 @@ import lombok.Getter;
 import java.util.List;
 
 @Getter
-public class MarsRover {
+class MarsRover {
     private Location location;
 
-    public MarsRover(int x, int y, Direction direction) {
+    MarsRover(int x, int y, Direction direction) {
         this.location = new Location(x, y, direction);
     }
 
-    public Location execute(List<Instruction> instructions) {
+    Location execute(List<Instruction> instructions) {
+
         return location;
     }
 }
--- src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
@@ -3,6 +3,9 @@ package com.thoughtworks.marsrover;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MarsRoverTest {
     @Test
     public void should_init_a_mars_rover_with_location_and_direction() {
@@ -11,5 +14,16 @@ public class MarsRoverTest {
         Assert.assertEquals(location.getX(), 0);
         Assert.assertEquals(location.getY(), 0);
         Assert.assertEquals(location.getDirection(), Direction.N);
+    }
+
+    @Test
+    public void should_Move_1_when_face_N_and_get_single_M() {
+        MarsRover marsRover = new MarsRover(0,0, Direction.N);
+        List<Instruction> instructions = new ArrayList<>();
+        instructions.add(Instruction.M);
+        Location location = marsRover.execute(instructions);
+        Assert.assertEquals(location.getDirection(), Direction.N);
+        Assert.assertEquals(location.getX(), 0);
+        Assert.assertEquals(location.getY(), 1);
     }
 }
