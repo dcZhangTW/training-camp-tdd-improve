96d1e4662eb4a47a7924bdead76a195fa979b2c0
dcZhangTW <dczhang@thoughtworks.com>
2020-01-13 16:45:38 +0800

    [Test-4] rotate R 90 for single instruction

--- src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
@@ -48,4 +48,15 @@ public class MarsRoverTest {
         Assert.assertEquals(location.getX(), 0);
         Assert.assertEquals(location.getY(), 0);
     }
+
+    @Test
+    public void should_Rotate_R_90_single_instruction() {
+        MarsRover marsRover = new MarsRover(0,0, Direction.N);
+        List<Instruction> instructions = new ArrayList<>();
+        instructions.add(Instruction.R);
+        Location location = marsRover.execute(instructions);
+        Assert.assertEquals(location.getDirection(), Direction.E);
+        Assert.assertEquals(location.getX(), 0);
+        Assert.assertEquals(location.getY(), 0);
+    }
 }
